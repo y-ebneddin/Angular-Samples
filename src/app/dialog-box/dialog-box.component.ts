b8416c32e92ef8d42b71dca562cfe2aed1c65e59
y-ebneddin <y_ebneddin@yahoo.com>
2023-04-18 13:45:08 +0200

    3 files modified

--- src/app/dialog-box/dialog-box.component.ts
+++ src/app/dialog-box/dialog-box.component.ts
@@ -13,13 +13,11 @@ export class DialogBoxComponent implements OnInit {
   public showYesNo: boolean = true;
   public dynamicColor: any;
   public dialogType: DialogType = DialogType.Info;
-  public buttons: DialogButton = DialogButton.Ignore;
+  public dialogButtons: DialogButton = DialogButton.OK;
 
   constructor(
     public dialogRef: MatDialogRef<DialogBoxComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) {
-    // , private translate: TranslatePipe
+    @Inject(MAT_DIALOG_DATA) public data: any) {  //, private translate: TranslatePipe
     this.title = data.title;
     this.message = data.message;
     this.showYesNo = data.showYesNo ?? true;
@@ -28,114 +26,98 @@ export class DialogBoxComponent implements OnInit {
       this.dialogType = data.dialogType;
 
     // *******************************************************************************************************************
-    //    Default value of dialog type. you can customize buttons and title in data.buttons and data.title
+    //  Default value of dialog type. 
+    //  You can customize dialogButtons and title in data.dialogButtons and data.title in constructor
     // *******************************************************************************************************************
     switch (this.dialogType) {
       case DialogType.Info:
-        this.dynamicColor = 'dialog-info';
-        this.buttons =
-          data.buttons != null || data.buttons != undefined
-            ? data.buttons
-            : DialogButton.OK;
-        this.title =
-          data.title != null || data.title != undefined
-            ? data.title
-            : 'Inforamtion';
+        this.dynamicColor = "dialog-info";
+        this.dialogButtons = (data.dialogButtons != null || data.dialogButtons != undefined) ? data.dialogButtons : DialogButton.OK;
+        this.title = (data.title != null || data.title != undefined) ? data.title : "Information";
         break;
       case DialogType.Confirm:
-        this.dynamicColor = 'dialog-confirm';
-        this.buttons =
-          data.buttons != null || data.buttons != undefined
-            ? data.buttons
-            : DialogButton.YesNo;
-        this.title =
-          data.title != null || data.title != undefined
-            ? data.title
-            : 'Confirmation';
+        this.dynamicColor = "dialog-confirm";
+        this.dialogButtons = (data.dialogButtons != null || data.dialogButtons != undefined) ? data.dialogButtons : DialogButton.YesNo;
+        this.title = (data.title != null || data.title != undefined) ? data.title : "Confirmation";
         break;
       case DialogType.Warning:
-        this.dynamicColor = 'dialog-warning';
-        this.buttons =
-          data.buttons != null || data.buttons != undefined
-            ? data.buttons
-            : DialogButton.OK;
-        this.title =
-          data.title != null || data.title != undefined
-            ? data.title
-            : 'Warning';
+        this.dynamicColor = "dialog-warning";
+        this.dialogButtons = (data.dialogButtons != null || data.dialogButtons != undefined) ? data.dialogButtons : DialogButton.AbortRetryIgnore;
+        this.title = (data.title != null || data.title != undefined) ? data.title : "Warnning";
         break;
       case DialogType.Error:
-        this.dynamicColor = 'dialog-error';
-        this.buttons =
-          data.buttons != null || data.buttons != undefined
-            ? data.buttons
-            : DialogButton.OK;
-        this.title =
-          data.title != null || data.title != undefined ? data.title : 'Error';
+        this.dynamicColor = "dialog-error";
+        this.dialogButtons = (data.dialogButtons != null || data.dialogButtons != undefined) ? data.dialogButtons : DialogButton.AbortRetryIgnore;
+        this.title = (data.title != null || data.title != undefined) ? data.title : "Error";
         break;
     }
 
-    // console.log('dialogType:', this.dialogType);
-    // console.log('buttons:', this.buttons);
-    // console.log('title:', this.title);
-    // console.log('dynamicColor:', this.dynamicColor);
+    // console.log('dialogButtons:', Object.keys(DialogButton).filter((item) => { return isNaN(Number(item)) }));
+    // console.log('dialogButtons:', Object.values(DialogButton).filter((item) => { return this.checkButtons(Number(item)) }).map(p => p as DialogButton));
+    // console.log('dialogButtons:', Object.values(DialogButton).filter((item) => { return this.checkButtons(Number(item)) && (Number(item) != 0 && (Number(item) & (Number(item) - 1)) == 0) }).map(p => DialogButton[p]));
   }
 
-  ngOnInit() {}
+  public getDialogButtons(): DialogButton[] {
+    return Object.values(DialogButton).filter((item) => {
+      return this.checkExistButtons(Number(item)) // check in selected dialogButtons list
+        && (Number(item) != 0 && (Number(item) & (Number(item) - 1)) == 0) // check single values
+    }).map(p => p as DialogButton);
+  }
 
-  public GetDialogButtons(): DialogButton[] {
-    return Object.values(DialogButton)
-      .filter((item) => {
-        return (
-          this.checkButtons(Number(item)) && // check in selected buttons list
-          Number(item) != 0 &&
-          (Number(item) & (Number(item) - 1)) == 0
-        ); // check single values
-      })
-      .map((p) => p as DialogButton);
+  public getDialogButtonName(buttonType: DialogButton): string {
+    return DialogButton[buttonType];
   }
 
   public get DialogButton() {
-    return DialogButton;
+    return DialogButton; 
   }
 
   public get DialogType() {
-    return DialogType;
+    return DialogType; 
   }
 
-  public checkButtons(buttonType: DialogButton): boolean {
-    return buttonType === (this.buttons & buttonType);
+  public checkExistButtonInSource(soureceButtons: DialogButton, buttonType: DialogButton) : boolean
+  {
+    return buttonType === (soureceButtons & buttonType);
   }
 
-  onClick(clickButton: DialogButton): void {
+  public checkSingleValue(soureceButtons: DialogButton) : boolean
+  {
+    return (Number(soureceButtons) != 0 && (Number(soureceButtons) & (Number(soureceButtons) - 1)) == 0);
+  }
+
+  private checkExistButtons(buttonType: DialogButton) : boolean
+  {
+    return buttonType === (this.dialogButtons & buttonType);
+  }
+
+  protected onClick(clickButton: DialogButton): void {
     switch (clickButton) {
+
       case DialogButton.No:
         this.dialogRef.close(DialogResult.No);
-        break;
-      case DialogButton.Abort:
-        this.dialogRef.close(DialogResult.Abort);
-        break;
-      case DialogButton.Cancel:
-        this.dialogRef.close(DialogResult.Cancel);
-        break;
-      case DialogButton.Ignore:
-        this.dialogRef.close(DialogResult.Ignore);
-        break;
-      case DialogButton.OK:
-        this.dialogRef.close(DialogResult.OK);
-        break;
-      case DialogButton.Retry:
-        this.dialogRef.close(DialogResult.Retry);
         break;
       case DialogButton.Yes:
         this.dialogRef.close(DialogResult.Yes);
         break;
-      case DialogButton.Confirm:
-          this.dialogRef.close(DialogResult.OK);
+      case DialogButton.Cancel:
+        this.dialogRef.close(DialogResult.Cancel);
         break;
-      case DialogButton.Continue:
-          this.dialogRef.close(DialogResult.Continue);
-          break;
+      case DialogButton.OK:
+        this.dialogRef.close(DialogResult.OK);
+        break;
+      case DialogButton.Abort:
+        this.dialogRef.close(DialogResult.Abort);
+        break;
+      case DialogButton.Retry:
+        this.dialogRef.close(DialogResult.Retry);
+        break;
+      case DialogButton.Ignore:
+        this.dialogRef.close(DialogResult.Ignore);
+        break;
+      case DialogButton.Confirm:
+        this.dialogRef.close(DialogResult.Confirm);
+        break;
       default:
         this.dialogRef.close(DialogResult.None);
         break;
--- src/app/dialog-box/dialog-types.enum.ts
+++ src/app/dialog-box/dialog-types.enum.ts
@@ -1,34 +1,47 @@
-export enum DialogType {
+ 
+export enum DialogType
+{
   Info,
   Warning,
   Error,
-  Confirm,
+  Confirm
 }
+  
 
+// *******************************************************************************************************************
+//  You can use hybrid buttons in code. Ex.: { dialogButtons: DialogButton.Abort | DialogButton.Retry }
+// *******************************************************************************************************************
 export enum DialogButton {
   Yes = 1 << 1,
   No = 1 << 2,
-  Cancel = 1 << 3,
-  Abort = 1 << 4,
-  Retry = 1 << 5,
-  Ignore = 1 << 6,
-  OK = 1 << 7,
+  OK = 1 << 3,
+  Cancel = 1 << 4,
+  Abort = 1 << 5,
+  Retry = 1 << 6,
+  Ignore = 1 << 7,
   Confirm = 1 << 8,
-  Continue = 1 << 9,
+
   YesNo = Yes | No,
   YesNoCancel = Yes | No | Cancel,
   OkCancel = OK | Cancel,
+  AbortRetry = Abort | Ignore,
+  AbortRetryIgnore = Abort | Retry | Ignore,
 }
 
+// *******************************************************************************************************************
+//  DialogResult.None : Not selected any button
+//  I think, DialogResult are not the same as DialogButton because some of buttons are the same meaning
+//  OK => OK, Yes, Confirm
+// *******************************************************************************************************************
 export enum DialogResult {
   None,
   Yes,
   No,
+  OK,
   Cancel,
   Abort,
   Retry,
   Ignore,
-  OK,
   Confirm,
-  Continue,
 }
+  
